36cd2c307bc6154abd465eaa8b4fa7b945c6d8b9
jlbraulio <jl.braulio@gmail.com>
2024-12-07 03:48:56 -0600

    Starting microservices project

---
+++ src/test/java/com/aplication/products/ProductsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplication.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
